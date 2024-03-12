bb58012877aac4675c9240104954831d60beb660
Vedant Agrawal <vedantagrawal647@gmail.com>
2024-03-13 00:20:00 +0530

    Added my project

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ app.post('/addUser',async (req,res) => {
          return  res.status(422).json({error:"user  already exist"});
       }
       else{
+        
           const user = new User({firstName,lastName,phoneNumber,countryCode,id});
 
           await user.save();
@@ -60,6 +61,57 @@ app.post('/addUser',async (req,res) => {
   }
 
 })
+
+app.put('/updateUser',async (req,res) => {
+  try{
+    const {firstName,lastName,phoneNumber,countryCode,id} = req.body;
+
+      if(!firstName|| !lastName|| !phoneNumber || !countryCode || !id){
+          return res.status(422).json({error:"please fill the form correctly"});
+      }
+
+      const userInfo = await User.findOne({id:id});
+
+      if(userInfo){
+
+          const result = await User.findByIdAndUpdate({_id},{
+            $set : {
+                firstName : firstName,lastName:lastName,phoneNumer:phoneNumber,countryCode:countryCode
+            }
+        },{
+            new:true,//now we get updated value in output
+            useFindAndModify:false
+        });
+         return  res.status(200).json({message:"data updated"});
+      }
+      else{
+        const user = new User({firstName,lastName,phoneNumber,countryCode,id});
+
+          await user.save();
+          res.status(200).json({messgae:"add user successfull"});
+
+      }
+    //findByIdANdUpdate == first find and then update
+    
+}
+catch(err){
+    console.log(err);
+}
+
+})
+
+app.delete('/deleteUser',async (req,res) => {
+  try{
+    const {id} = req.body;
+    const result = await User.findByIdAndDelete({id});
+    res.status(200).json({messgae:"delete user successfull"});
+  }
+  catch(err){
+    console.log(err);
+  }
+})
+
+
 
 // Start the server
 app.listen(PORT, () => {
